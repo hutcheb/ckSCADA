095c9e7c42a584fb102b24834b542de55901aa24
Ben Hutcheson <ben.hutche@gmail.com>
2020-06-28 04:44:40 -0400

    Removed the fs dependancy line in the server package.json.
    It seemed to reference a not needed package.

--- ckscada-server/server/src/simulate_load_test.js
+++ ckscada-server/server/src/simulate_load_test.js
@@ -10,13 +10,13 @@ function sleep(ms) {
 
 async function running() {
   for (i=0; i<5; i++) {
-    ckdevice.readConfigFile("Simulated_Load_Test_" + i, "simulation", 1000, "../../config/test.json", false, true)
+    ckdevice.readConfigFile("Simulated_Load_Test_" + i, "simulation", 1000, "../../config/config.json", false, true)
   }
   await sleep(10000);
 
   for (i=0; i<5; i++) {
     for (j=0; j<1000; j++) {
-      cktag.createTag("Test_Tag_Name_" + j, "Simulated_Load_Test_" + i, "int", "Description Text", 0, i, 0, j, "%", "../../config/test.json", false, true)
+      cktag.createTag("Test_Tag_Name_" + j, "Simulated_Load_Test_" + i, "int", "Description Text", 0, i, 0, j, "%", "../../config/config.json", false, true)
       await sleep(10);
     }
   }
